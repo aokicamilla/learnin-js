6d8b3389f4ec8d2938c7caefe67c27ed6d93f132
Camilla Aoki <caokicontato@gmail.com>
2024-03-14 16:22:29 -0300

    aprendendo sobre promises

--- node-lib/index.js
+++ node-lib/index.js
@@ -1,18 +1,29 @@
+import fs from 'fs';
 import chalk from 'chalk';
 
-console.log(chalk.blue('ola mundo'));
+function trataErro(erro) {
+    console.log(erro)
+    throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório'));
+}
 
-console.log(chalk.blue.bgWhite.bold('Alura'));
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8'
+    fs.promises
+        .readFile(caminhoDoArquivo, encoding)
+        .then((texto) => console.log(chalk.green(texto)))
+        .catch((erro) => trataErro(erro))
+}
 
-console.log(chalk.blue('curso', 'de', 'Node.js'));
-//métodos aninhados
-console.log(chalk.red('vermelho', chalk.underline.bgBlue('azul')));
-// uso de template strings e placeholders
-console.log(`
-CPU: ${chalk.red('90%')}
-RAM: ${chalk.green('40%')}
-DISK: ${chalk.yellow('70%')}
-`);
 
-// console.log('São geralmente recuperados a partir de um objeto [FileList] (https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/');
-// console.log('São geralmente recuperados a partir de um objeto [FileList] (https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento <input> (https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer] (https://developer.mozilla.org/pt-BR/docs/Web/API/');
+//Método síncrono
+// function pegaArquivo (caminhoDoArquivo) {
+//     const encoding = 'utf-8'
+//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//         if(erro) {
+//             trataErro(erro);
+//         }
+//         console.log(chalk.green(texto))
+//     })
+// }
+
+pegaArquivo('./arquivos/texto.md')
