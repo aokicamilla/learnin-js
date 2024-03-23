a3c88f860ef1d9d6d1a8e5ff6b19940fd3ddcbbd
Camilla Aoki <caokicontato@gmail.com>
2024-03-23 00:44:39 -0300

    encapsulamento, polimorfismo e solid

--- POO/User-class.js
+++ POO/User-class.js
@@ -11,14 +11,64 @@ export default class User {
         this.#role = role || 'estudante'
         this.#ativo = ativo
     }
+    //as subclasses não conseguem acessar os campos privados, para isso temos que usar métodos de acesso
+    //GET é um método de leitura, metodo para acessar pelas subclasses devem fazer apenas uma coisa, evitando acoplar tudo e podendo acessar apenas um campo, 
+
+    get nome() {
+        return this.#nome
+    }
+
+    get email() {
+        return this.#email
+    }
+
+    get nascimento() {
+        return this.#nascimento
+    }
+
+    get role() {
+        return this.#role
+    }
+
+    get ativo() {
+        return this.#ativo
+    }
+
+    //set é usado só para dentro do encapsulamento, para que esteja disponível alterar fora dessa classe
+
+    set ativo(novoAtivo) {
+        this.#ativo = novoAtivo
+    }
+
+    set nome (novoNome) {
+        if (novoNome === '') {
+            throw new Error ('Preencha o nome')
+        }
+        this.#nome = novoNome
+    }
 
     exibirInfos() {
-        return `${this.#nome}, ${this.#email}`
+        return `${this.nome}, ${this.email}, ${this.ativo}`
     }
 }
 
-const novoUser = new User ('Julia', 'j@j.com', '04/05/2000')
 
+
+//para acessar o método privado só podemos acessar dentro desta classe podemos assim criar um método
+    // #montaObjUser() {
+    //     return ({
+    //         nome: this.#nome,
+    //         email: this.#email,
+    //         nascimento: this.#nascimento,
+    //         role: this.#role,
+    //         ativo: this.#ativo
+    //     })
+    // }
+//portanto essa função passa o resultado do método privado para uma const objUser.
+
+
+//TESTES
+// const novoUser = new User ('Julia', 'j@j.com', '04/05/2000')
 // console.log(novoUser)
 // console.log(novoUser.exibirInfos())
 // console.log(User.prototype.isPrototypeOf(novoUser))
--- POO/index.js
+++ POO/index.js
@@ -4,7 +4,13 @@ import Docente from './Docente.js'
 
 const novoUser = new User('Mara', 'm@m.com', '25/06/2000')
 
-console.log(novoUser.exibirInfos())
-
 //dessa forma não conseguimos trocar os dados
 // novoUser.#nome = 'Sheila'
+
+const novoAdmin = new Admin ('Rodrigo', 'r@r.com', '23/04/2001')
+
+
+console.log(novoAdmin.nome)
+novoAdmin.ativo = false
+novoAdmin.nome = 'Lucila'
+console.log(novoAdmin.exibirInfos())
